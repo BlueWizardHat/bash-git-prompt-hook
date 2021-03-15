9ceb274509be44439b0ce7f5fa195cf7284fc580
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2021-03-15 20:02:26 +0100

    nicer start of prompt

--- bash-smart-prompt.sh
+++ bash-smart-prompt.sh
@@ -36,8 +36,8 @@ ps1_calc_git_len() {
 
 PS1_TITLE='\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}'
 PS1_CLEARLINE='$(printf "%$((columns - 1))s\r\e[K\]")'
-PS1_LINE1_L='\[\e[0;34m\]| $(ps1_exit_code) \[\e[0;36m\]${timer_show} $([ \j -gt 0 ] && echo -e "\[\e[0;33m\]\j ")\[\e[0;34m\]\t \[\e[0;33m\]\w'
+PS1_LINE1_L='\[\e[0;34m\]┌ $(ps1_exit_code) \[\e[0;36m\]${timer_show} $([ \j -gt 0 ] && echo -e "\[\e[0;33m\]\j ")\[\e[0;34m\]\t \[\e[0;33m\]\w'
 PS1_LINE1_R='$git_prompt_line'
-PS1_LINE2='\[\e[0;34m\]| \[\e[0;32m\]\u\[\e[0;34m\]@\[\e[0;32m\]\h \[\e[01;33m\]\$\[\e[0m\] '
+PS1_LINE2='\[\e[0;34m\]└ \[\e[0;32m\]\u\[\e[0;34m\]@\[\e[0;32m\]\h \[\e[01;33m\]\$\[\e[0m\] '
 
 PS1="$PS1_TITLE$PS1_CLEARLINE$PS1_LINE1_L$PS1_LINE1_R\n$PS1_LINE2"
