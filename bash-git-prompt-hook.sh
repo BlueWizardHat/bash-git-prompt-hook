7bebdf850a729f6dafac9b67450daed1e2c60a04
Mads Hvelplund <mads@adamatics.com>
2022-01-31 13:33:58 +0000

    Fixed rebasing error

--- bash-git-prompt-hook.sh
+++ bash-git-prompt-hook.sh
@@ -124,7 +124,7 @@ function git_bash_prompt() {
 
 	# Find the origin
 	local origin_raw=$(git config --get remote.origin.url 2> /dev/null) || true
-	origin=$(echo "$origin" | sed -e 's/%[0-9a-f][0-9a-f]/¤/ig') # Replace URL encode entitites with a ¤ mark
+	origin_raw=$(echo "$origin_raw" | sed -e 's/%[0-9a-f][0-9a-f]/¤/ig') # Replace URL encode entitites with a ¤ mark
 	if [ -z "$origin_raw" ]; then
 		origin_raw="[no origin]"
 		origin="${color_origin}[no origin]"
